7701b4687bcc6c58ab578f30ba88836237cad78b
JaznanOfficial <jaznanofficial@gmail.com>
2021-08-21 15:06:24 +0600

    also some changes

--- main.js
+++ main.js
@@ -2,67 +2,65 @@
 let bestPrice = document.getElementById('best-price');
 let bestPriceNumber = bestPrice.innerText;
 bestPriceNumber = parseFloat(1299);
-console.log(typeof(bestPriceNumber));
 
 
 // memory cost fucntion
 
 document.getElementById('default-memory').addEventListener('click', function () {
-    let extMemory = document.getElementById('ext-memory-cost')
-    let extMemoryNumber = extMemory.innerText;
+    let extMemory = document.getElementById('extra-memory-cost')
     extMemory.innerText = 0;
-    console.log(typeof(parseInt(extMemory.innerText)));
-    return parseInt(extMemoryNumber);;
+    extMemoryNumber = parseInt(extMemory.innerText);
+    return extMemoryNumber;
 })
 document.getElementById('memory16').addEventListener('click', function () {
-    let extMemory = document.getElementById('ext-memory-cost')
-    let extMemoryNumber = extMemory.innerText;
+    let extMemory = document.getElementById('extra-memory-cost')
     extMemory.innerText = 180;
-    console.log(typeof(parseInt(extMemory.innerText)));
-    return parseInt(extMemoryNumber);
+    extMemoryNumber = parseInt(extMemory.innerText);
+    return extMemoryNumber;
 })
 
 // extra storage cost function
 document.getElementById('default-storage-btn').addEventListener('click', function () {
     let extMemory = document.getElementById('extra-storage-cost')
-    let extMemoryNumber = extMemory.innerText;
     extMemory.innerText = 0;
-    console.log(typeof(parseInt(extMemory.innerText)));
-    return parseInt(extMemoryNumber);
+    extMemoryNumber = parseInt(extMemory.innerText);
+    return extMemoryNumber;
 })
 document.getElementById('second-ssd').addEventListener('click', function () {
     let extMemory = document.getElementById('extra-storage-cost')
-    let extMemoryNumber = extMemory.innerText;
     extMemory.innerText = 100;
-    console.log(typeof(parseInt(extMemory.innerText)));
-    return parseInt(extMemoryNumber);
+    extMemoryNumber = parseInt(extMemory.innerText);
+    return extMemoryNumber;
 })
 document.getElementById('third-ssd').addEventListener('click', function () {
     let extMemory = document.getElementById('extra-storage-cost')
-    
-    let extMemoryNumber = extMemory.innerText;
     extMemory.innerText = 180;
-    console.log(typeof(parseInt(extMemory.innerText)));
-    return parseInt(extMemoryNumber);
+    extMemoryNumber = parseInt(extMemory.innerText);
+    return extMemoryNumber;
 })
 
 
 // delivery cost function
 document.getElementById('regular-shipping').addEventListener('click', function () {
     let extMemory = document.getElementById('delivery-cost')
-    let extMemoryNumber = extMemory.innerText;
     extMemory.innerText = 0;
-    console.log(typeof(parseInt(extMemory.innerText)));
-    return parseInt(extMemoryNumber);
+    extMemoryNumber = parseInt(extMemory.innerText);
+    return extMemoryNumber;
 })
 document.getElementById('urgent-shipping').addEventListener('click', function () {
     let extMemory = document.getElementById('delivery-cost')
-    let extMemoryNumber = extMemory.innerText;
     extMemory.innerText = 20;
-    console.log(typeof(parseInt(extMemory.innerText)));
-    return parseInt(extMemoryNumber);
+    extMemoryNumber = parseInt(extMemory.innerText);
+    return extMemoryNumber;
 })
 
 // total price
+function total(a,b,c) {
+    let totalPrice = a + b + c;
+    document.getElementById('total-price').innerText = totalPrice;
+    return totalPrice
+}
 
-document.getElementById('total-price').innerText = 
+total
+
+
